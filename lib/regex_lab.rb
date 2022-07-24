bd948c56ec908c757af9d717fea6e3b05c0f2978
Jackie <jye0325@gmail.com>
2022-07-24 16:16:00 -0400

    commit tree 16fde158e073042e548a2fd548822abbb69fe0f9

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/\b[aeiou]/i)
+        true
+    else 
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/(un\S*ing)/).flatten
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/(\b\S{5}\b)/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/\A[A-Z]/) && text.match(/[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}$/) ? true : false
 end
